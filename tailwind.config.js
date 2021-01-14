3c515dd96b3014db16b6e7e5e4ac357089939410
George Brown <teawares@gmail.com>
2021-01-14 17:34:33 +0800

    new way to  define tailwind 4/5vh

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@ module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      height: {
-        "screen-1/2": "50vh",
-        "screen-1/4": "25vh",
-        "screen-3/4": "75vh",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
