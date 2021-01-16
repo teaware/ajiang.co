fa4f30701e815f0abc52fc40190bebd41c3058b5
George Brown <teawares@gmail.com>
2021-01-16 22:04:10 +0800

    📚 menu component & playing adjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      xm: { min: "140px", max: "288px" },
+    },
     extend: {
       keyframes: {
         wave: {
