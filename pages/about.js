1e0f51603abbab26a3effdabcb5563873a544be6
George Brown <teawares@gmail.com>
2021-02-03 17:18:07 +0800

    🐶 instant push

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>阿江</title>
+      </Head>
+
+      <div className="h-screen">
+        <h1>hello</h1>
+        <div></div>
+      </div>
+    </>
+  );
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Blog() {
+  return (
+    <>
+      <Head>
+        <title>阿江 | Blog</title>
+      </Head>
+
+      <div className="h-screen">
+        <h1>Blog</h1>
+        <div></div>
+      </div>
+    </>
+  );
+}
