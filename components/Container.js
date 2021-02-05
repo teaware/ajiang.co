8a3d51fa670793fb0cf1d0a01479dd8c1cb0c46d
George Brown <teawares@gmail.com>
2021-02-06 00:52:55 +0800

    💫 menu

--- components/Container.js
+++ components/Container.js
@@ -1,8 +1,145 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useTheme } from "next-themes";
 import NextLink from "next/link";
+
+import { motion, useCycle } from "framer-motion";
+
+const vMenu = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${
+      height * 3 + 200
+    }px at 2rem calc(2rem + env(safe-area-inset-top)))`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+    zIndex: 0,
+  }),
+  closed: {
+    clipPath: "circle(1px at 2rem calc(2rem + env(safe-area-inset-top)))",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+    transitionEnd: { zIndex: -1 },
+  },
+};
+
+const vItems = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const Items = () => (
+  <motion.ul
+    className="absolute top-0 left-0 bottom-0 w-full flex flex-col items-center justify-center"
+    variants={vItems}
+  >
+    {links.map((link) => (
+      <Item key={link.name} {...link} />
+    ))}
+  </motion.ul>
+);
+const links = [
+  { to: "/", name: "Home" },
+  { to: "/blog", name: "Blog" },
+  { to: "/about", name: "About" },
+];
+
+const vItem = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+const Item = (link) => {
+  return (
+    <motion.li
+      className="flex items-center w-60 h-10 mb-6 cursor-pointer"
+      variants={vItem}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <NextLink href={link.to}>
+        <a>{link.name}</a>
+      </NextLink>
+    </motion.li>
+  );
+};
+
+const Path = (props) => (
+  <motion.path
+    className="stroke-current stroke-2"
+    strokeLinecap="round"
+    {...props}
+  />
+);
+
+const MenuToggle = ({ toggle }) => (
+  <button
+    onClick={toggle}
+    className="w-8 h-8 p-1 rounded-full flex items-center justify-center select-none outline-none focus:outline-none border-none"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 -2 23 23"
+      className="w-6 h-6 text-gray-600 dark:text-white"
+    >
+      <Path
+        variants={{
+          closed: { d: "M 2 2.5 L 20 2.5" },
+          open: { d: "M 3 16.5 L 17 2.5" },
+        }}
+      />
+      <Path
+        d="M 2 9.423 L 20 9.423"
+        variants={{
+          closed: { opacity: 1 },
+          open: { opacity: 0 },
+        }}
+        transition={{ duration: 0.1 }}
+      />
+      <Path
+        variants={{
+          closed: { d: "M 2 16.346 L 20 16.346" },
+          open: { d: "M 3 2.5 L 17 16.346" },
+        }}
+      />
+    </svg>
+  </button>
+);
+
+const useDimensions = (ref) => {
+  const dimensions = useRef({ width: 0, height: 0 });
+
+  useEffect(() => {
+    dimensions.current.width = ref.current.offsetWidth;
+    dimensions.current.height = ref.current.offsetHeight;
+  }, []);
+
+  return dimensions.current;
+};
 
 export default function Container(props) {
   const [mounted, setMounted] = useState(false);
@@ -20,6 +157,10 @@ export default function Container(props) {
     type: "website",
     ...customMeta,
   };
+
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
 
   return (
     <>
@@ -45,11 +186,11 @@ export default function Container(props) {
       </Head>
 
       <div className="flex flex-col w-full min-h-screen font-sans text-base antialiased text-gray-800 bg-white dark:text-white dark:bg-gray-600">
-        <nav className="absolute top-0 left-1/2 transform -translate-x-1/2 flex justify-between items-center text-base max-w-4xl w-full px-4 py-2 lg:py-4 mt-safe-top mx-auto z-10">
+        <nav className="absolute top-0 left-1/2 transform -translate-x-1/2 flex justify-between items-center text-base max-w-4xl w-full p-4 mt-safe-top z-10">
           <a href="#skip" className="sr-only focus:not-sr-only">
             Skip to content
           </a>
-          <div>
+          <div className="hidden sm:block">
             <NextLink href="/">
               <a className="p-1 sm:p-4 text-gray-900 dark:text-gray-100">
                 Home
@@ -66,6 +207,15 @@ export default function Container(props) {
               </a>
             </NextLink>
           </div>
+
+          <motion.div
+            className="sm:hidden"
+            initial={false}
+            animate={isOpen ? "open" : "closed"}
+          >
+            <MenuToggle toggle={() => toggleOpen()} />
+          </motion.div>
+
           <button
             aria-label="Toggle Dark Mode"
             type="button"
@@ -75,7 +225,7 @@ export default function Container(props) {
             {mounted && (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+                viewBox="-1 -1 24 24"
                 fill="currentColor"
                 className="h-7 w-7 text-gray-600 dark:text-white"
               >
@@ -93,6 +243,20 @@ export default function Container(props) {
           </button>
         </nav>
         <main id="skip">{children}</main>
+        <motion.div
+          className="menu"
+          initial={false}
+          animate={isOpen ? "open" : "closed"}
+          custom={height}
+          ref={containerRef}
+        >
+          <motion.div
+            className="w-full h-full max-h-screen bg-indigo-300 dark:bg-indigo-800 absolute top-0 left-0 bottom-0 pt-safe-top box-content"
+            variants={vMenu}
+          >
+            <Items />
+          </motion.div>
+        </motion.div>
       </div>
     </>
   );
