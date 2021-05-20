336d1938c37e5ecc5147d89dcc88e2a799519abe
George Brown <teawares@gmail.com>
2021-05-20 17:58:55 +0800

    🤏🏻 edit a bit

--- components/Container.js
+++ components/Container.js
@@ -284,9 +284,12 @@ export default function Container(props) {
               <motion.svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                fill="currentColor"
+                fill="none"
                 variants={vRotate}
                 transition={trans}
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="h-6 w-6 text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
               >
                 <mask id="moon-mask">
@@ -303,6 +306,7 @@ export default function Container(props) {
                 <motion.circle
                   variants={vCCircle}
                   transition={trans}
+                  fill="currentColor"
                   cx="12"
                   cy="12"
                   r="9"
@@ -311,81 +315,17 @@ export default function Container(props) {
 
                 <motion.g
                   variants={vLine}
-                  transition={trans}
+                  // transition={trans}
                   stroke="currentColor"
                 >
-                  <line
-                    x1="12"
-                    y1="1"
-                    x2="12"
-                    y2="3"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="12"
-                    y1="21"
-                    x2="12"
-                    y2="23"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="4.22"
-                    y1="4.22"
-                    x2="5.64"
-                    y2="5.64"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="18.36"
-                    y1="18.36"
-                    x2="19.78"
-                    y2="19.78"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="1"
-                    y1="12"
-                    x2="3"
-                    y2="12"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="21"
-                    y1="12"
-                    x2="23"
-                    y2="12"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="4.22"
-                    y1="19.78"
-                    x2="5.64"
-                    y2="18.36"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <line
-                    x1="18.36"
-                    y1="5.64"
-                    x2="19.78"
-                    y2="4.22"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                  <line x1="12" y1="1" x2="12" y2="3" />
+                  <line x1="12" y1="21" x2="12" y2="23" />
+                  <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                  <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                  <line x1="1" y1="12" x2="3" y2="12" />
+                  <line x1="21" y1="12" x2="23" y2="12" />
+                  <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                  <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
                 </motion.g>
               </motion.svg>
             </button>
