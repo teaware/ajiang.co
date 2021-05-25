3fe846ef356bcf522dfa6759d33d9630d4951d37
George Brown <teawares@gmail.com>
2021-05-25 10:35:48 +0800

    👾 50%

--- components/Container.js
+++ components/Container.js
@@ -143,7 +143,7 @@ export default function Container(props) {
                 fill="currentColor"
                 variants={vRotate}
                 transition={trans}
-                style={{ originX: 0.5, originY: 0.5 }}
+                style={{ originX: '50%', originY: '50%' }}
                 className="h-6 w-6 text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
               >
                 <mask id="moon-mask">
