20caa2824405d85b1a04bfb744bfca691f2e4b02
George Brown <teawares@gmail.com>
2022-06-28 17:20:52 +0800

    🪺 blog

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <Container title="about – 阿江" description="我爱大自然">
       <div className="flex flex-col justify-center items-start max-w-2xl px-5 pt-safe-top mx-auto my-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mt-4 md:mt-8 mb-16">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight my-4 md:mt-8">
           About
         </h1>
         <p className="mb-4">我本花一朵🌷</p>
--- pages/blog.js
+++ pages/blog.js
@@ -24,42 +24,6 @@ export default function Blog({ posts }) {
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight my-4 md:mt-8">
           Blog
         </h1>
-        <p className="mb-4">{`总共 ${posts.length} 篇`}</p>
-        <div className="relative w-full mb-4">
-          <input
-            aria-label="Search articles"
-            type="text"
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Search articles"
-            className="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-          />
-          <svg
-            className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </div>
-        {!searchValue && (
-          <>
-            {/* <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
-              Most Popular
-            </h3>
-            <p>something hide when search</p> */}
-          </>
-        )}
-        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
-          {searchValue ? 'Result' : 'All Posts'}
-        </h3>
-        {!filteredBlogPosts.length && 'No posts found.'}
         {filteredBlogPosts.map((frontMatter) => (
           <BlogPost key={frontMatter.title} {...frontMatter} />
         ))}
