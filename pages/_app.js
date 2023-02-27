9b2915913778f7e45ccc75928f4cd19c2f4047fb
George Brown <teawares@gmail.com>
2023-02-28 00:39:25 +0800

    :airplane: <Analytics />

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
-import '@/styles/global.css';
+import '@/styles/global.css'
 
-import { ThemeProvider } from 'next-themes';
-import { MDXProvider } from '@mdx-js/react';
-import MDXComponents from '@/components/MDXComponents';
+import { ThemeProvider } from 'next-themes'
+import { MDXProvider } from '@mdx-js/react'
+import MDXComponents from '@/components/MDXComponents'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
         <Component {...pageProps} />
+        <Analytics />
       </MDXProvider>
     </ThemeProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
